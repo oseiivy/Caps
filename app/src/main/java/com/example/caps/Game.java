db559bf66d3eec8fcf6166144544fd329d654880
Ivy Osei <oseiivy@my.yorku.ca>
2020-03-26 19:01:49 -0400

    Caps commit

---
+++ app/src/main/java/com/example/caps/Game.java
@@ -0,0 +1,51 @@
+package e.o.caps;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import ca.roumani.i2c.Country;
+import ca.roumani.i2c.CountryDB;
+
+public class Game {
+    CountryDB db;
+
+
+
+    public Game(){
+        this.db = new CountryDB();
+    }
+
+    public String qa(){
+        List<String> capitals = db.getCapitals();
+        // Randomly Pick a country from data base of countries
+        int n = capitals.size();
+        int index = (int)(n*Math.random()) ;
+        // from the randomly chosen capitals it makes c the name of the capital
+        String c = capitals.get(index);
+        //we get the refence data base for all the countries
+        Map<String, Country> data = db.getData();
+        Country ref = data.get(c);
+        String question;
+
+
+        if (Math.random() < 0.5){
+            question =  "What is the capital of " + ref.getName() +"?" + "\n" + ref.getCapital();
+
+
+        }
+        else {
+            question =  ref.getCapital() + " is the capital of?" + "\n" + ref.getName();
+        }
+
+
+
+        return question;
+    }
+
+    public static void main(String[] args) {
+        Game myModel = new Game();
+        System.out.println(myModel.qa());
+
+    }
+}
